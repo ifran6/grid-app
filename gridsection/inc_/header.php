c68ef8e4b81509271823c7f2e2864d5c697f6704
ifran6 <zeelink2016@gmail.com>
2025-05-30 11:16:37 +0100

    new update

---
+++ gridsection/inc_/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$pageName." | ".$pageTopic ?></title>
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/pure-grid.css">
+</head>
+<body>
+<div class="pg-nav">
+    <a href="index.php" class="m-nav">Home</a>
+    <a href="flex-set.php" class="m-nav">Flex-System</a>
+    <a href="display-block-set.php" class="m-nav">Display Block</a>
+    <a href="grid.php" class="m-nav">Grid-Portal</a>
+</div>
